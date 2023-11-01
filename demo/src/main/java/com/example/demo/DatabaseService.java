a430ac82609cc222e86832771f30379498f6104e
IsaacUlate <isaaculate2@gmail.com>
2023-10-31 19:20:48 -0600

    Files

---
+++ demo/src/main/java/com/example/demo/DatabaseService.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.util.List;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+ 
+@Service
+public class DatabaseService {
+ 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+ 
+    public boolean isDatabaseConnected() {
+        try {
+            jdbcTemplate.queryForObject("SELECT 1 FROM DUAL", Integer.class);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+ 
+        public boolean queryData() {
+        try {
+           List<String> myList = jdbcTemplate.queryForList("SELECT logMessage FROM checkinlove_next_gen.app_log", String.class);
+           System.out.println(myList);
+           return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+    }
+}
